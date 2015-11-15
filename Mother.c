082b28ee9508e30520f4e1298035041bd912ce18
pianonerd7 <pianonerd7@gmail.com>
2015-11-15 16:35:34 -0500

    mother implemented

--- Mother.c
+++ Mother.c
@@ -1,1 +1,31 @@
+#include <time.h>
+#include <stdlib.h>
+#include <unistd.h>
+#include <string.h>
 #include "Cookie.x"
+
+#define MAX_COOKIES 20
+
+int NumOfTinaCookie = 0;
+int cookies = MAX_COOKIES;
+
+int getCookie(int userID) {
+
+	if (cookies < 1) {
+		return -2
+	}
+
+	//1 is judy
+	if (useID == 1) {
+		if (NumOfTinaCookie < 2) {
+			return -1;
+		}
+		cookies--;
+		return 1;
+	}
+	//0 is tina
+	else if (userID == 0) {
+		cookies--;
+		return 1;
+	}
+}
